5ef1172a46f18dd2b9ea1fee47757b0eacdf8068
Hang Chen <hanglearning@gmail.com>
2022-11-23 17:03:09 -0500

    some updates

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import pickle
+import sys
 
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
@@ -38,24 +39,23 @@ models_base_folder = "/Users/chenhang/Downloads/models_4_mal"
 def main():
 
     # populate selections
-    rainable_layers, clients, selectable_rounds = get_selections(models_base_folder)
+    clients, selectable_rounds = get_selections(models_base_folder)
 
     # get selections from front-end
-    client1 = request.form.get('clients1') if request.form.get('clients1') else "globals_0"
-    client2 = request.form.get('clients2') if request.form.get('clients2') else "client_7"
-    round1 = request.form.get('rounds1') if request.form.get('rounds1') else 0
-    round2 = request.form.get('rounds2') if request.form.get('rounds2') else 1
-    top_or_low = request.form.get('top_or_low') if request.form.get('rounds2') else 'top'
+    clientA = request.form.get('clientsA') if request.form.get('clientsA') else "globals_0"
+    clientB = request.form.get('clientsB') if request.form.get('clientsB') else "client_7"
+    roundA = request.form.get('roundsA') if request.form.get('roundsA') else 0
+    roundB = request.form.get('roundsB') if request.form.get('roundsB') else 1
+    top_or_low = request.form.get('top_or_low') if request.form.get('roundsB') else 'top'
     percent = float(request.form.get('percent')) if request.form.get('percent') else 0.2
 
-    nn1_path = f"{models_base_folder}/{client1}/{round1}.pkl"
-    nn2_path = f"{models_base_folder}/{client2}/{round2}.pkl"
-    top_or_low_indicator = 3 if top_or_low == 'top' else 1
+    nn1_path = f"{models_base_folder}/{clientA}/{roundA}.pkl"
+    nn2_path = f"{models_base_folder}/{clientB}/{roundB}.pkl"
     
     
-    left_figs = display_weights_single_net(nn1_path, 1, top_or_low_indicator, percent)
-    middle_figs = compare_weights_change(nn1_path, nn2_path, top_or_low_indicator, percent)
-    right_figs = display_weights_single_net(nn2_path, 2, top_or_low_indicator, percent)
+    left_figs = display_weights_single_net(nn1_path, 1, top_or_low, percent)
+    middle_figs = compare_weights_change(nn1_path, nn2_path, top_or_low, percent)
+    right_figs = display_weights_single_net(nn2_path, 2, top_or_low, percent)
 
     
     left_layer_to_plot_data = construct_layer_to_plot_data(left_figs)
@@ -63,14 +63,11 @@ def main():
     right_layer_to_plot_data = construct_layer_to_plot_data(right_figs)
 
     return render_template('index.html', 
-                            rainable_layers = rainable_layers, clients = clients, selectable_rounds = selectable_rounds, 
-                            client1 = client1, client2 = client2, round1 = round1, round2 = round2, top_or_low = top_or_low, percent = percent,
+                            clients = clients, selectable_rounds = selectable_rounds, 
+                            clientA = clientA, clientB = clientB, roundA = roundA, roundB = roundB, top_or_low = top_or_low, percent = percent,
                             left_layer_to_plot_data = left_layer_to_plot_data, middle_layer_to_plot_data = middle_layer_to_plot_data, right_layer_to_plot_data = right_layer_to_plot_data)
 
 def get_selections(models_base_folder):
-    # get trainable layers 
-    with open(f"{models_base_folder}/globals_0/0.pkl", 'rb') as f:
-        ref_nn_layer_to_weights = pickle.load(f)
     
     # get clients
     clients = [name for name in os.listdir(models_base_folder) if os.path.isdir(os.path.join(models_base_folder, name))]
@@ -79,8 +76,8 @@ def get_selections(models_base_folder):
     # get available comm rounds
     selectable_rounds = [f.split('.')[0] for f in listdir(f"{models_base_folder}/globals_0") if isfile(join(f"{models_base_folder}/globals_0", f))]
     selectable_rounds.sort(key=int)
-
-    return list(ref_nn_layer_to_weights.keys()), clients, selectable_rounds
+   
+    return clients, selectable_rounds
 
 def construct_layer_to_plot_data(layer_to_figs):
     layer_to_data = []
@@ -92,13 +89,14 @@ def construct_layer_to_plot_data(layer_to_figs):
         layer_to_data.append(data)
     return layer_to_data
 
-def normalize_weights(nn_path, percent=0.2):
+
+def normalize_weights(nn_path, top_or_low, percent=0.2):
 
     """ Transforms weights matrix to numbers:
-        Top percent - 3
-        Middles - 2
-        Low percent - 1
+        Top/Low weights - 2
+        Others - 1
         Pruned - (keep) 0
+        Padding - -10
 
     Returns:
         layer_to_matrix _dict_: key: layer, value: transformed matrix
@@ -116,52 +114,56 @@ def normalize_weights(nn_path, percent=0.2):
         # take abs as we show magnitude values
         param_1d_array = np.absolute(param_1d_array)
 
-        # calculate low percent (involving pruned weights)
         pruned_percent = round(param_1d_array[param_1d_array == 0].size/param_1d_array.size, 1)
-        low_percent = percent + pruned_percent
+        
+        # if need to display low, this is a trick
+        display_percent = percent + pruned_percent if top_or_low == 'low' else percent
+            
+        percent_order = math.ceil(param_1d_array.size * display_percent)
 
-        top_percent_order = math.ceil(param_1d_array.size * percent)
-        low_percent_order = math.ceil(param_1d_array.size * low_percent)
+        if top_or_low == 'top':
+            percent_threshold = np.partition(param_1d_array, -percent_order)[-percent_order]
+        elif top_or_low == 'low':
+            percent_threshold = np.partition(param_1d_array, percent_order - 1)[percent_order - 1]
 
-        low_percent_threshold = np.partition(param_1d_array, low_percent_order - 1)[low_percent_order - 1]
-        top_percent_threshold = np.partition(param_1d_array, -top_percent_order)[-top_percent_order]
-
-        # reshape flatterned NN to 2D array and pad with -10 (originally as nan, but introduced difficulty in color mapping)
+        # reshape flatterned NN to 2D array and pad with nan
         side_len = math.ceil(math.sqrt(param_1d_array.size))
 
         param_2d_array = np.pad(param_1d_array.astype(float), (0, side_len*side_len - param_1d_array.size), 
-            mode='constant', constant_values=-10).reshape(side_len,side_len)
+            mode='constant', constant_values=np.nan).reshape(side_len,side_len)
 
         display_2d_array = np.empty_like(param_2d_array)
-        display_2d_array[:] = -10
-
-        # change middle elements to 2
-        display_2d_array[np.where((param_2d_array > low_percent_threshold) & (param_2d_array < top_percent_threshold))] = 2
-
-        # change low weights to 1
-        display_2d_array[np.where((0 < param_2d_array) & (param_2d_array <= low_percent_threshold))] = 1
-
-        # change top weights to 3, may overwrite low weights when network reaches target pruning rate
-        display_2d_array[np.where(param_2d_array >= top_percent_threshold)] = 3
+        display_2d_array[:] = -10 # start from all paddings
 
         # keep pruned weights 0
         display_2d_array[param_2d_array == 0] = 0
+
+        if top_or_low == 'top':
+            # change top weights to 2
+            display_2d_array[np.where(param_2d_array >= percent_threshold)] = 2
+            # change other weights to 1
+            display_2d_array[np.where(param_2d_array < percent_threshold)] = 1
+            # keep pruned weights 0, may overwrite some top weights if (1 - specified top show percent) > pruned_percent
+            display_2d_array[param_2d_array == 0] = 0
+        elif top_or_low == 'low':
+            # change low weights to 2
+            display_2d_array[np.where(param_2d_array <= percent_threshold)] = 2
+            # change other weights to 1
+            display_2d_array[np.where(param_2d_array > percent_threshold)] = 1
+            # keep pruned weights 0, may overwrite some low weights
+            display_2d_array[param_2d_array == 0] = 0
 
         layer_to_matrix[layer.split(".")[0]] = display_2d_array.astype(int)
 
     return layer_to_matrix
 
-def save_fig_to_disk():
-    pass
-
-def display_weights_single_net(nn_path, nn1_or_nn2, top_or_low = 3, percent=0.2):
+def display_weights_single_net(nn_path, nn1_or_nn2, top_or_low = "top", percent=0.2):
 
     """ 
     nn1_or_nn2 -> int: 1 or 2
-    top_or_low -> 1 or 3
     """
 
-    nn_layer_to_matrix = normalize_weights(nn_path, percent)
+    nn_layer_to_matrix = normalize_weights(nn_path, top_or_low, percent)
 
     layer_to_figs = {}
 
@@ -175,14 +177,10 @@ def display_weights_single_net(nn_path, nn1_or_nn2, top_or_low = 3, percent=0.2)
         ax = fig.subplots()
 
         color_map = {0: np.array([0, 0, 0]), # black - pruned
-                     2: np.array([255, 255, 255]), # white - values in between,
-                     -10: np.array([255, 255, 255]), # white - padding,
-                    top_or_low: weight_color_rgb}
-        
-        if top_or_low == 1:
-            color_map[3] = np.array([255, 255, 255])
-        else:
-            color_map[1] = np.array([255, 255, 255])
+                     1: np.array([247, 228, 194]), # skin color - other weights,
+                     2: weight_color_rgb, # top/low weights, highlighted,
+                     -10: np.array([255, 255, 255]) # white - padding,
+                   }
 
         # make a 3d numpy array that has a color channel dimension   
         data_3d = np.ndarray(shape=(weights.shape[0], weights.shape[1], 3), dtype=int)
@@ -192,42 +190,51 @@ def display_weights_single_net(nn_path, nn1_or_nn2, top_or_low = 3, percent=0.2)
                     data_3d[i][j] = color_map[weights[i][j]]
                 except:
                     print(weights)
+                    sys.exit("Error in coloring.")
+                    
 
         c = ax.matshow(data_3d,
                     interpolation ='nearest',
                     aspect='auto',
                         origin ='lower')
 
-        client_index = nn_path.split("/")[-2]
+        # calculate percentage of highlights
+        size_without_padding = weights[weights != -10].size
+        pruned_percent = weights[weights == 0].size/size_without_padding
+        other_percent = weights[weights == 1].size/size_without_padding
+        highlighted_percent = weights[weights == 2].size/size_without_padding
         
         # fig.colorbar(c, ax = ax)
-        top_or_low_indicator = 'Top' if top_or_low == 3 else 'Low'
-        patch_0 = mpatches.Patch(color='white', label=f'Padding')
-        patch_1 = mpatches.Patch(color='black', label=f'Pruned')
-        patch_2 = mpatches.Patch(color=weight_color, label=f'{top_or_low_indicator} {percent:.0%}')
+        patch_padding = mpatches.Patch(color='white', label=f'Padding')
+        patch_0 = mpatches.Patch(color='black', label=f'Pruned {pruned_percent:.0%}')
+        patch_1 = mpatches.Patch((247/256, 228/256, 194/256), label=f'Others {other_percent:.0%}')
+        patch_2 = mpatches.Patch(color=weight_color, label=f'{top_or_low.title()} {highlighted_percent:.0%}')
 
-        ax.legend(handles=[patch_0, patch_1, patch_2])
+        ax.legend(handles=[patch_padding, patch_0, patch_1, patch_2])
 
-        fig.suptitle(f"{layer} - {client_index} {top_or_low_indicator} {percent:.0%}", fontsize=20)
+        client_index = nn_path.split("/")[-2]
+        fig.suptitle(f"{layer} - {client_index} {top_or_low.title()} {percent:.0%}", fontsize=20)
 
-        fig.savefig(f"{nn_path.split('.')[0]}.png")
+        # fig.savefig(f"{nn_path.split('.')[0]}.png")
         
         layer_to_figs[layer] = fig
 
     return layer_to_figs
 
-def compare_weights_change(nn_1_path, nn_2_path, top_or_low = 3, percent=0.2):
+def compare_weights_change(nn_1_path, nn_2_path, top_or_low, percent=0.2):
     """
     Input: 
     nn_1_path is typically the pkl path of global_model or local_model_1
     nn_2_path is typically the pkl path of local_model or local_model_2
-    top_or_low: 3 or 1; 3 for top percent, 1 for low percent
+    Output:
+        plot display_arr: -1, 0, 1, 2, 3 - see color map
+                        
     """
 
     # fig, ax = plt.subplots(figsize=(18, 10))
 
-    nn_1_layer_to_matrix = normalize_weights(nn_1_path, percent)
-    nn_2_layer_to_matrix = normalize_weights(nn_2_path, percent)
+    nn_1_layer_to_matrix = normalize_weights(nn_1_path, top_or_low, percent)
+    nn_2_layer_to_matrix = normalize_weights(nn_2_path, top_or_low, percent)
 
     layer_to_figs = {}
 
@@ -236,9 +243,9 @@ def compare_weights_change(nn_1_path, nn_2_path, top_or_low = 3, percent=0.2):
         fig = Figure()
         ax = fig.subplots()
 
-        same_weights = np.logical_and(weights == top_or_low, nn_2_layer_to_matrix[layer] == top_or_low) + 0
-        nn1_unique = np.logical_and(weights == top_or_low, nn_2_layer_to_matrix[layer] != top_or_low) + 0
-        nn2_unique = np.logical_and(weights != top_or_low, nn_2_layer_to_matrix[layer] == top_or_low) + 0
+        same_weights = np.logical_and(weights == 2, nn_2_layer_to_matrix[layer] == 2) + 0
+        nn1_unique = np.logical_and(weights == 2, nn_2_layer_to_matrix[layer] == 1) + 0
+        nn2_unique = np.logical_and(weights == 1, nn_2_layer_to_matrix[layer] == 2) + 0
 
 
         nn2_unique[nn2_unique == 1] = 3
@@ -246,18 +253,20 @@ def compare_weights_change(nn_1_path, nn_2_path, top_or_low = 3, percent=0.2):
         nn1_unique[nn1_unique == 1] = 1
         display_arr = nn2_unique + same_weights + nn1_unique
 
+        # show pruned weights as well
+        display_arr[weights == 0] = -1
+        display_arr[nn_2_layer_to_matrix[layer] == 0] = -1
+
         # calculate change percentage
-        entire_counts = (weights == top_or_low).sum()
-        if entire_counts == 0:
-            # when model reaches target pruning rate, low(1) are overwritten by top(3) so entire_counts will be 0
-            entire_counts = (weights == 3).sum() # -> LOW still has bug, DO NOT USE
+        entire_counts = (weights == 2).sum() # same amount for both networks, due to same percent
         nn2_perc = (nn2_unique == 3).sum()/entire_counts
         same_perc = (same_weights == 2).sum()/entire_counts
         nn1_perc = (nn1_unique == 1).sum()/entire_counts
         
         # https://stackoverflow.com/questions/37719304/python-imshow-set-certain-value-to-defined-color
         # define color map 
-        color_map = {0: np.array([255, 255, 255]), # while - nothing
+        color_map = {-1: np.array([0, 0, 0]), #  black - pruned
+                    0: np.array([255, 255, 255]), # white - nothing
                     1: np.array([0, 0, 255]), # blue - nn1_unique
                     2: np.array([255, 0, 0]), # red - same
                     3: np.array([0, 255, 0])} # lime - nn2_unique 
@@ -277,19 +286,19 @@ def compare_weights_change(nn_1_path, nn_2_path, top_or_low = 3, percent=0.2):
 
         
         # fig.colorbar(c, ax = ax)
-        top_or_low_indicator = 'Top' if top_or_low == 3 else 'Low'
+        patch_padding = mpatches.Patch(color='white', label=f'Padding')
+        patch_0 = mpatches.Patch(color='black', label=f'Pruned')
         patch_1 = mpatches.Patch(color='lime', label=f'NN2 - {nn2_perc:.2%}')
         patch_2 = mpatches.Patch(color='red', label=f'Same - {same_perc:.2%}')
         patch_3 = mpatches.Patch(color='blue', label=f'NN1 - {nn1_perc:.2%}')
 
-        ax.legend(handles=[patch_1, patch_2, patch_3])
+        ax.legend(handles=[patch_padding, patch_0, patch_1, patch_2, patch_3])
 
-        fig.suptitle(f"{layer} - {top_or_low_indicator} {percent:.0%}", fontsize=20)
+        fig.suptitle(f"{layer} - {top_or_low.title()} {percent:.0%}", fontsize=20)
         # fig.savefig(f'/Users/chenhang/Downloads/weight_change/{layer}.png')
         layer_to_figs[layer] = fig
 
     return layer_to_figs
 
 if __name__ == "__main__":
-    # hello()
     app.run(debug=True)
